3d569a5b239b229baea8ddefb6408353b98917a2
chimeratech-py <xkenntsx619@gmail.com>
2023-01-02 15:08:18 +0800

    Finished build vocab button functionality

--- trytkinter.py
+++ trytkinter.py
@@ -1,8 +1,13 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 from unittest import skip
 from weakref import ref
 from nltk.translate.bleu_score import sentence_bleu
 import spacy
+import tkinter.scrolledtext as st
+import os
+import subprocess
+import time
 
 def getPOS(inputtext):
     c = 0
@@ -98,7 +103,7 @@ def compare_POS(referencetxt, candidatetxt):
 
 
 
-class Window:
+# class fr2(tk.Frame):
     def __init__(self, master):
         self.nlp = spacy.load('en_core_web_sm')
         
@@ -127,7 +132,7 @@ class Window:
         self.reflbl = Label(subframe, text = "Reference Text")
         self.reflbl.place(relx=0.5,anchor=N)
         self.reftxt = Label(subframe, text = self.ref_being_evaluated)
-        self.reftxt.place(relx=0.5, rely=0.5,anchor=CENTER)
+        self.reftxt.place(relx=0.5, rely=0.5,anchor='center')
         subframe.pack(expand = True, fill = BOTH, side=LEFT)
         
         
@@ -135,27 +140,27 @@ class Window:
         
         
        
-        subframe2 = Frame(master, background="red")
-        self.candlbl = Label(subframe2, text="Candidate Text")
+        subframe2 = ttk.Frame(master, background="red")
+        self.candlbl = ttk.Label(subframe2, text="Candidate Text")
         self.candlbl.place(relx=0.5,anchor=N)
-        self.candtxt = Label(subframe2, text= self.cand_being_evaluated)
+        self.candtxt = ttk.Label(subframe2, text= self.cand_being_evaluated)
         self.candtxt.place(relx=0.5, rely=0.5,anchor=CENTER) 
         
         bleuscore = sentence_bleu(list(self.ref_being_evaluated), list(self.cand_being_evaluated), weights=(1, 0, 0, 0))
         
         dictOutput = structure_evaluation(self.nlp(self.ref_being_evaluated),self.nlp(self.cand_being_evaluated), bleuscore)
         
-        self.bleu_lbl = Label(subframe2, text = 'standard bleu score: ' + str(bleuscore))
+        self.bleu_lbl = ttk.Label(subframe2, text = 'standard bleu score: ' + str(bleuscore))
         self.bleu_lbl.place(relx=0.5, rely=0.6,anchor=CENTER)
-        self.gross_lbl = Label(subframe2, text = 'Structure sensitive Bleu score: ' + str(dictOutput['grs']))
+        self.gross_lbl = ttk.Label(subframe2, text = 'Structure sensitive Bleu score: ' + str(dictOutput['grs']))
         self.gross_lbl.place(relx=0.5, rely=0.7, anchor = CENTER)
-        self.corr_lbl = Label(subframe2, text = f'Correctly placed POS tags:' +  str(dictOutput['correctly placed tags']))
+        self.corr_lbl = ttk.Label(subframe2, text = f'Correctly placed POS tags:' +  str(dictOutput['correctly placed tags']))
         self.corr_lbl.place(relx=0.5, rely=0.8, anchor = CENTER)
-        self.compare_lbl = Label(subframe2, text = f'Number of correctly placed tags: {compare_POS(self.nlp(self.ref_being_evaluated), self.nlp(self.cand_being_evaluated))}')
+        self.compare_lbl = ttk.Label(subframe2, text = f'Number of correctly placed tags: {compare_POS(self.nlp(self.ref_being_evaluated), self.nlp(self.cand_being_evaluated))}')
         self.compare_lbl.place(relx=0.5, rely=0.9, anchor = CENTER)
         subframe2.pack(expand=True, fill=BOTH, side=LEFT)
         
-        self.btn_next = Button(subframe,text="Next",command =self.updateTexts)
+        self.btn_next = ttk.Button(subframe,text="Next",command =self.updateTexts)
         self.btn_next.place(relx=0.5, rely=0.8, anchor = CENTER)
         
     def updateTexts(self):
@@ -178,11 +183,110 @@ class Window:
         # self.subject.config(text=reftxt)
         # self.message.config(text=candtxt)
             
+class Display:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry('900x900')
+        
+        self.root.title("GUI Tabs Class")
+        
+        self.tabControl = ttk.Notebook(self.root)
+        
+        self.tab1 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tab1, text="Translation")
+        
+        self.text_area = st.ScrolledText(self.tab1,
+                            width = 50, 
+                            height = 25, 
+                            font = ("Times New Roman",
+                                    15))
+        self.text_area.grid(row = 0, column = 0, rowspan= 3)
+        self.text_area.configure(state ='disabled')
+        
+        def buildVocab(textwidget):
+            textwidget.configure(state='normal')
+            with open('vocabInfo.txt','r') as file:
+                t = file.read()
+            
+            time.sleep(5)
+            textwidget.insert(tk.INSERT, t)
+            
+            textwidget.configure(state='disabled')
+            
+            # os.chdir('Collab files')
+            # returned_value = os.system(r'cmd /k onmt_build_vocab -config en_tl.yaml -n_sample 10000')
+            # print('returned value: ', returned_value)
+            
+        self.btn_vocab = ttk.Button(self.tab1,text="Build Vocab",command=lambda: buildVocab(self.text_area))
+        self.btn_vocab.grid(row = 0, column= 1)
+        
+        def onmtTrain(textwidget):
+            textwidget.configure(state='normal')
+            textwidget.delete('1.0',tk.END)
+            textwidget.insert(tk.INSERT, 'updated')
+            textwidget.configure(state='disabled')
+            ##returned_value = os.system(r'cmd /k onmt_train -config en_tl.yaml')
+            ##print('returned value: ', returned_value)
+        
+        self.btn_train = ttk.Button(self.tab1, text="Train", command=lambda: onmtTrain(self.text_area))
+        self.btn_train.grid(row=1, column = 1)
+        
+        self.btn_translate = ttk.Button(self.tab1, text="Translate")
+        self.btn_translate.grid(row=2, column = 1)
+        
+        self.tab2 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tab2, text="Evaluation")
+        
+        self.nlp = spacy.load('en_core_web_sm')
+        
+        self.counter=0
+        
+        self.ref_list = []
+        with open('reference.txt',mode='r') as re:
+            for line in re:
+                self.ref_list.append(line)
+        
+        self.ca_list = []
+        with open('candidate.txt',mode='r') as f:
+            for line in f:
+                self.ca_list.append(line)
+                
+        self.ref_being_evaluated = self.ref_list[self.counter]
+        self.cand_being_evaluated = self.ca_list[self.counter]
+        
+        
+        self.reflbl = ttk.Label(self.tab2, text = "Reference Text")
+        self.reflbl.place(relx=0.5,anchor='n')
+        self.reftxt = ttk.Label(self.tab2, text = self.ref_being_evaluated)
+        self.reftxt.place(relx=0.5, rely=0.5,anchor='center')
+        
+        
+        self.tabControl.pack(expand=1, fill="both")
+        
+        
+        
+        self.root.mainloop()
         
         
             
-root = Tk()
-root.geometry('300x200')
-window = Window(root)
+# root = tk.Tk()
+# root.geometry('300x200')
+# tabControl = ttk.Notebook(root)
 
-root.mainloop()  
+# tab1 = ttk.Frame(tabControl)
+# tab2 = ttk.Frame(tabControl)
+
+# tabControl.add(tab1, text ='Tab 1')
+# tabControl.add(tab2, text ='Tab 2')
+# tabControl.pack(expand = 1, fill ="both")
+
+# ttk.Label(tab1, 
+#           text ="Welcome to \
+#           GeeksForGeeks").grid(column = 0, 
+#                                row = 0,
+#                                padx = 30,
+#                                pady = 30)  
+          
+# root.mainloop()  
+
+display = Display()
